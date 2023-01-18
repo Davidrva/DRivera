84f404feee4705c08636cd8fa2fe811f3e57a107
David Rivera <alberto.rivera@epn.edu.ec>
2023-01-18 18:12:17 -0500

    [Davidrva]:Se han quitado lineas de codigo
    se ha realizado con exito

--- Salida.java
+++ Salida.java
@@ -2,15 +2,14 @@ public class Salida
 {
     public static void main(String[] args)
     {
-        System.out.println("Hola usuario bienvenido");
-        System.out.println("A continuación se muestra una cuenta regresiva");
+        
 
         for (int i = 5; i >= 1;  i--)
         {
             System.out.println(i);
         }
 
-        System.out.println("Ahora, otra cuenta regresiva");
+        
 
         for (int i = 5; i >= 1;  i--)
         {
